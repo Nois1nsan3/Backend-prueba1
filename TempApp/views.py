39c2112c91efe0de065403fca4d427041d3aeb8b
Nois1nsan3 <108773158+Nois1nsan3@users.noreply.github.com>
2023-05-06 17:33:50 -0400

    testeo

--- TempApp/views.py
+++ TempApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def celulares(request):
+    return render (request, 'celulares.html')
+
+
+def juegos(request):
+    return render (request, 'juegos.html')
+
