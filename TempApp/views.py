17f435c09faf4e7fa1e63d2187d85fcf1d17703a
Nois1nsan3 <108773158+Nois1nsan3@users.noreply.github.com>
2023-05-06 22:58:58 -0400

    cuarto commit

--- TempApp/views.py
+++ TempApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Template, Context
+
 
         
 
@@ -17,5 +19,22 @@ def juegos(request):
     return render (request, 'templates/juegos.html')
 
 def usuario(request):
-    return HttpResponse ("Bienvenido a la pagina de usuario")
+    doc_externo=open('C:/Users/boriz/OneDrive/Escritorio/Django proyectos/Django_Prueba_1/plantillas/templates/usuario.html')
+
+    plt = Template(doc_externo.read())
+
+    doc_externo.close()
+
+    ctx=Context({
+        "title" : "DJANGO SHOP",
+        "user" : "Datos de Usuario",
+        "nombre" : "Xavier",
+        "apellido": "Papito",
+        "nacionalidad" : "Hindi",
+        "return" : "Volver",
+        })
+
+    pagina = plt.render(ctx)
+
+    return HttpResponse(pagina)
 
